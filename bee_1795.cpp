68d894512cc43e9cfbe3f535b8b6c2c444babdea
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-03-17 22:16:39 +0600

    Add files via upload

---
+++ bee_1795.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n; long long value;
+    cin >> n;
+    value = pow(3,n);
+    cout << value << endl;
+    return 0;
+}
