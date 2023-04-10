ce12409966cdea35c0039024f7c88c998422eb72
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-04-10 22:32:23 +0600

    Add files via upload

---
+++ bee_1847.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if(a<b && b>=c) cout << ":(" << endl;
+    else if(a>b && b<=c) cout << ":)" << endl;
+    else if((b>a && c>b) && b-a <= c-b) cout << ":)" << endl;
+    else if((b>a && c>b) && b-a >= c-b) cout << ":(" << endl;
+    else if((b<a && c<b) && a-b > b-c) cout << ":)" << endl;
+    else if((b<a && c<b) && a-b <= b-c) cout << ":(" << endl;
+    else if(a==b && b<c) cout << ":)" << endl;
+    else if(a==b && b>c) cout << ":(" << endl;
+    else cout << ":(" << endl;
+    return 0;
+}
