b78c2990c4e7a89a0288e9006c89ec4aad3d95f9
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-04-10 23:18:36 +0600

    Update bee_1847.cpp

--- bee_1847.cpp
+++ bee_1847.cpp
@@ -8,7 +8,7 @@ int main()
     if(a<b && b>=c) cout << ":(" << endl;
     else if(a>b && b<=c) cout << ":)" << endl;
     else if((b>a && c>b) && b-a <= c-b) cout << ":)" << endl;
-    else if((b>a && c>b) && b-a >= c-b) cout << ":(" << endl;
+    else if((b>a && c>b) && b-a > c-b) cout << ":(" << endl;
     else if((b<a && c<b) && a-b > b-c) cout << ":)" << endl;
     else if((b<a && c<b) && a-b <= b-c) cout << ":(" << endl;
     else if(a==b && b<c) cout << ":)" << endl;
